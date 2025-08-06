75ef46d7e8892d965a7c4db823646b7a31e1e7b9
Ritik27-r <rawatritik70@gmail.com>
2025-08-06 23:06:07 +0530

    third commit

---
+++ 4.1_array_methods.js
@@ -0,0 +1,28 @@
+var a =[1,2,3,4,5];
+//length
+console.log(a.length);
+
+//indexOf()
+console.log(a.indexOf(2));
+
+//includes()
+console.log(a.includes(4));
+
+//push()
+a.push("A");
+console.log(a);
+a.push("B","C")
+
+//pop()
+a.pop();
+console.log(a);
+
+//unshift()-push but does it at the front
+a.unshift(0);
+console.log(a)
+
+//shift()
+a.shift();
+console.log(a);
+
+//splice()
